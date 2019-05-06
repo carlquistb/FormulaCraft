17340434f81038941c8857dfe9f760c1613c3aea
Brendan Carlquist <carlquistb@gmail.com>
2019-05-06 00:38:09 -0700

    resolved #5, issue with pipes closing before child process terminates. Uses Systemd as a process manager. Issue not resolved: create new system image or install the service at startup.

--- AutoStack/mineShell.js
+++ AutoStack/mineShell.js
@@ -7,7 +7,7 @@
 
 	This script will handle boot up and boot down of the .jar file.
 
-	This script will not handle boot up and boot down of stacks of 
+	This script will not handle boot up and boot down of autoStacks.
 	
 
 */
@@ -16,43 +16,21 @@ const fs = require('fs'); //fileserver library
 const spawn = require('child_process').spawn; //this function creates a child process (basically another shell for Minecraft to run in)
 
 //spawn a child_process to run java. reference: child_process.spawn(command[, args][, options])
-var child = spawn('java',['-jar','server.jar','nogui'],{cwd: '/home/ec2-user/mc',stdio: ['pipe','pipe','pipe']});
+const child = spawn('java',['-jar','server.jar','nogui'],{cwd: '/home/ec2-user/mc',stdio: ['pipe','pipe','pipe']});
 
 //set eventlisteners for each pipe received data. reference: child_process<EventEmitter>
-process.stdin.on('data', onData); //stdin for process
 child.stdout.on('data', onData); //stdout for subprocess
 child.stderr.on('data', onData); //stderr for subprocess
 child.on('exit', function onExit() {
-	console.log('minecraft server exited through onExit function in mineShell.js');
+	console.log('mcserver exited through onExit function in mineShell.js');
 	process.exit(0);
 });
 
-//event-listener function. Runs each time a server line-item is printed.
-function onData(data) { 
-	var str = String(data);
-	var exitPassword = /the answer is 42/;
-
-	console.log("DEBUG: data as string = " + String(data));
-	var datastring = String(data);
-	console.log("DEBUG: data as explicit string = " + datastring);
-
-	//if the player says the answer, save and exit the server.
-	if(String(data).match(exitPassword)) {
-		child.stdin.write('/stop\n');
-	}
-}
-
 //pipe data received from the shell into the subprocess, and from the subprocess through to the shell.
-process.stdin.pipe(child.stdin);
 child.stdout.pipe(process.stdout);
 child.stderr.pipe(process.stderr);
 
-//also, pipe all this into a log output file here.
-var logfile = fs.createWriteStream('mineShellLog.txt');
-process.stdin.pipe(logfile);
-child.stdout.pipe(logfile);
-child.stderr.pipe(logfile);
-
+/*
 //runs on a set interval.
 function recurringSave() {
 	child.stdin.write('/say hello world\n');
@@ -60,4 +38,18 @@ function recurringSave() {
 	child.stdin.write('/save-all\n');
 }
 
-setInterval(recurringSave,1000*10); //run the recurringSave method every 5 seconds.
+var saveInterval = setInterval(recurringSave,1000*10); //run the recurringSave method every 5 seconds.
+*/
+
+//event-listener function. Runs each time a server line-item is printed.
+function onData(data) {
+	console.log('onData entered');
+	//if the player says the answer, save and exit the server.
+	if(String(data).includes("the answer is 42")) {
+		//clearInterval(saveInterval);
+		child.stdin.write('/stop\n');
+	} else if(String(data).includes("I need help")) {
+		console.log(JSON.stringify(child.stdin));
+		child.stdin.write('/say your wish is my command\n');
+	}
+}
--- README.md
+++ README.md
@@ -7,27 +7,20 @@ A SMS-based service for running Minecraft server on AWS.
 
 This implementation will have two high level components. First, a AWS Cloudformation script that implements the SMS-based API for querying the status of servers being run. Second, a Cloudformation script that implements the stack for each server requested. We will work from the bottom up, hoping to develop the stack,  fully automated, first. Before the creation of this repo, significant work was done creating a working stack allowing for MCServer to run.
 
-# code
+# Resources
 
 ## .gitignore
 	- .pem, .ppk are both keys used for accessing ec2 instances through SSH on Windows.
 	- .txt are almost always my own simple note files. Not necessary for understanding this repo.
 
-## FormulaStack.yaml
-	- This Cloudformation script will generate the resources needed to run the API that will give and answer calls for Minecraft Servers. This stack should be left up for the duration of the project's use.
-## AutoStack.yaml
+## FormulaStack
+	- This Cloudformation stack will generate the resources needed to run the API that will give and answer calls for Minecraft Servers. This stack should be left up for the duration of the project's use.
+
+## AutoStack
 	- This Cloudformation script will create a proprietary stack for each Minecraft Server requested through the API. These stacks will be brought up and down by the API, and will not be generated manually at all, unless for debugging purposes.
 	- set up cronjob to automatically upload world over time.
 	- run mineShell.
 	- clean up save and close down stack...? (TODO: close down stack automation.)
-
-## mineShell.js
-	- This script is responsible for managing the server.jar as a child process. 
-	- user-initiated world save and exit
-	- automated world save (not upload- this will be a cronjob set up in AutoStack.)
-
-## cronjob
-	- this is a executable that will be installed by the EC2 userdata, and will be responsible for upload to s3 periodically.
 
 # dependencies
 
@@ -50,3 +43,5 @@ below this section, all of this is just notes for myself.
 
 ## resources
 	- https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-up-node-on-ec2-instance.html
+	- similar project from Positron: https://github.com/positron/aws-minecraft
+	- https://www.twilio.com/blog/2014/11/child-processes-streams-minecraft-multiplayer-server-nodejs.html
