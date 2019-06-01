35dab2fb2dbb07a4647abbe5a85a0219cf72fcdd
lipian <lipian@cs.washington.edu>
2019-05-31 20:14:36 -0700

    Streamlining the server starting process and adding some functionality to mineShell.js

--- AutoStack/mineShell.js
+++ AutoStack/mineShell.js
@@ -15,9 +15,27 @@
 const StreamWatcher = require('./StreamWatcher.js');
 const fs = require('fs'); //fileserver library
 const spawn = require('child_process').spawn; //this function creates a child process (basically another shell for Minecraft to run in)
+const execFile = require('child_process').execFile;
 
 //spawn a child_process to run java. reference: child_process.spawn(command[, args][, options])
 const child = spawn('java',['-jar','server.jar','nogui'],{cwd: '/home/ec2-user/mc',stdio: ['pipe','pipe','pipe']});
+
+function uploadWorld() {
+	log("Uploading world...");
+	execFile("/home/ec2-user/scripts/uploadworld", 
+	         ["world1"], 
+	         function (error, stdout, stderr) {
+		if (error) {
+			log("Problem uploading world: " + error);
+		} else if (stderr.length != 0) {
+			log("Error while uploading world: " + error);
+		}
+	});
+}
+
+function log(str) {
+	console.log("[mineShell] " + str);
+}
 
 //pipe data received from the shell into the subprocess, and from the subprocess through to the shell.
 child.stdout.pipe(process.stdout);
@@ -25,27 +43,26 @@ child.stderr.pipe(process.stderr);
 
 let streamWatcher = new StreamWatcher(child);
 streamWatcher.addOnExit(function () {
-	console.log('mcserver exited through onExit function in mineShell.js');
+	uploadWorld();
 	process.exit(0);
-})
+});
 
 let regex42 = /the answer is 42/;
 let regexHelp = /I need help/;
 
 streamWatcher.addWatcher(regex42, function (stdin, regexData) {
-	stdin.write('/say your wish is my command\n');
+	stdin.write("/say your wish is my command\n");
 });
 
 streamWatcher.addWatcher(regexHelp, function (stdin, regexData) {
-	stdin.write('/say your wish is my command\n');
+	stdin.write("/say your wish is my command\n");
 });
-/*
-//runs on a set interval.
-function recurringSave() {
-	child.stdin.write('/say hello world\n');
-	console.log("DEBUG: write hello world");
-	child.stdin.write('/save-all\n');
-}
 
-var saveInterval = setInterval(recurringSave,1000*10); //run the recurringSave method every 5 seconds.
-*/
+// The SIGTERM event will be sent by systemctl when the service is stopped
+process.on("SIGTERM", function () {
+	log("[mineShell] Shutting down server...");
+	child.stdin.write("say Server shutting down\n");
+	child.stdin.write("stop\n");
+});
+
+var saveInterval = setInterval(uploadWorld,1000*60*15); //run the recurringSave method every 15 minutes.
