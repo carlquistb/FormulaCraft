eb6e339728cc7687473a5b940c25270effcb77b6
Brendan Carlquist <carlquistb@gmail.com>
2019-06-30 23:13:06 -0700

    finnaly got low enough to retrieve the public IPv4!

--- FormulaStack/MineAPI/lambda/stacks--GET.js
+++ FormulaStack/MineAPI/lambda/stacks--GET.js
@@ -51,6 +51,59 @@ exports.handler = (event, context, callback) => {
             body: JSON.stringify(body)
         };
 
+        var summary;
+        for(summary of data.StackSummaries) {
+
+          //list stack resources
+          console.log('print id of each stackSummary: ' + summary.StackId);
+          var listResourceParams = {
+            StackName: summary.StackId
+          };
+
+          cfn.listStackResources(listResourceParams, function(err, data) {
+            if(err) {
+              console.log(err,err.stack);
+            }
+            else {
+              //find the resource with type AWS::EC2::SpotFleet
+              var resource;
+              for(resource of data.StackResourceSummaries){
+                if(resource.ResourceType == 'AWS::EC2::SpotFleet'){
+                  console.log(resource.PhysicalResourceId);
+                  //describe the instances inside the fleet
+                  var ec2 = new aws.EC2();
+                  params = {
+                    SpotFleetRequestId: resource.PhysicalResourceId
+                  };
+                  console.log('params:',resource.PhysicalResourceId);
+                  ec2.describeSpotFleetInstances(params,function(err,data) {
+                    if(err) console.log(err,err.stack);
+                    else {
+                      console.log(data.ActiveInstances);
+
+                      params = {
+                        InstanceIds: [
+                          data.ActiveInstances[0].InstanceId
+                        ]
+                      };
+
+                      ec2.describeInstances(params,function(err,data) {
+                        if(err) console.log(err,err.stack);
+                        else {
+                          console.log('describing instances');
+                          console.log(data);
+                          console.log(data.Reservations[0].Instances);
+                        }
+                      });
+                    }
+                  });
+                }
+              }
+            }
+          });
+
+        }
+
         callback(null, response);
       }
     });
