87e319f85b3f6082914213683c79c1e44ef6016f
Brendan Carlquist <carlquistb@gmail.com>
2019-08-17 16:37:08 -0700

    set onclick for refreshFlavors. Create injectFlavors.

--- FormulaStack/clientPortal/dashboard.js
+++ FormulaStack/clientPortal/dashboard.js
@@ -4,13 +4,20 @@ const API_URL = "https://lt175p2wc6.execute-api.us-west-2.amazonaws.com/v0-1";
 
 //set onclick functions.
 $("#refresh-worlds").click(refreshWorlds);
+$("#refresh-flavors").click(refreshFlavors);
 
 //onclick listener.
 //updates local cache for all data necessary for updating world display.
 //injects objects for worlds not currently displayed.
 function refreshWorlds() {
-  IconClickLoading(this, function(){
-    fetchFlavors().then(fetchWorlds).then(injectWorlds);
+  iconClickLoading(this, function(){
+    return fetchFlavors().then(fetchWorlds).then(injectWorlds);
+  });
+}
+
+function refreshFlavors() {
+  iconClickLoading(this, function() {
+    return fetchFlavors().then(injectFlavors);
   });
 }
 
@@ -18,12 +25,13 @@ function refreshWorlds() {
 // "loading/rowing" icon while your function is active.
 //
 // ONLY for use with click listeners on material icons.
-function IconClickLoading(onclickThis, toDo) {
+function iconClickLoading(onclickThis, toDo) {
   var element = onclickThis;
   var original = element.innerHTML;
   element.innerHTML = "rowing";
-  toDo();
-  element.innerHTML = original;s
+  toDo().then(function() {
+    element.innerHTML = original;
+  });
 }
 
 //obtain new flavors data.
@@ -118,6 +126,7 @@ function injectWorlds() {
 
       var cardHeader = document.createElement("div");
       cardHeader.classList.add("card-header");
+      cardHeader.setAttribute("data-background-color","white")
       cardHeader.appendChild(document.createTextNode(world.worldName));
       card.appendChild(cardHeader);
 
@@ -155,6 +164,80 @@ function injectWorlds() {
         cardFooterLink.href = "blah"; //TODO: find syntax.
         cardFooterLink.appendChild(document.createTextNode(
           "create stack with this world..."
+        ));
+        cardFooter.appendChild(cardFooterLink);
+  }
+}
+
+/*
+forms and injects DOM elements containing information on each world.
+removes and repopulates #worlds-cards-row.
+TODO: which is better, appendChild all at the end, or when the element is made?
+*/
+function injectFlavors() {
+
+  var flavors = JSON.parse(localStorage.flavors);
+
+  //delete current HTML
+  var row = document.getElementById("flavors-cards-row");
+  while(row.firstChild) {
+    row.removeChild(row.firstChild);
+  }
+
+  //for each flavor object in localStorage array
+  for(var i = 0; i < flavors.length; i++) {
+
+    var flavor = flavors[i];
+
+    //build new HTML
+    var column = document.createElement("div");
+    column.classList.add("col-lg-4","col-md-6","col-sm-6");
+    row.appendChild(column);
+
+    var card = document.createElement("div");
+    card.classList.add("card","card-stats");
+    column.appendChild(card);
+
+      var cardHeader = document.createElement("div");
+      cardHeader.classList.add("card-header");
+      cardHeader.setAttribute("data-background-color","white")
+      cardHeader.appendChild(document.createTextNode(flavor.flavorDescription));
+      card.appendChild(cardHeader);
+
+      var cardContent = document.createElement("div");
+      cardContent.classList.add("card-content");
+      card.appendChild(cardContent);
+
+        var cardContentCategory = document.createElement("p");
+        cardContentCategory.classList.add("category");
+        cardContentCategory.appendChild(document.createTextNode("MC version"));
+        cardContent.appendChild(cardContentCategory);
+
+        var cardContentTitle = document.createElement("p");
+        cardContentTitle.classList.add("card-title");
+        //TODO: find syntax.
+        cardContentTitle.appendChild(document.createTextNode(
+          flavor.minecraftVersion
+        ));
+        cardContent.appendChild(cardContentTitle);
+
+      var cardFooter = document.createElement("div");
+      cardFooter.classList.add("card-footer");
+      card.appendChild(cardFooter);
+
+        var cardFooterLink = document.createElement("a");
+        cardFooterLink.onclick = function() {
+          /*
+          createStackWithWorld(
+            world.worldName + "-" + getMonth() + "-" + getDate() + "-" + getYear(),  //stackName
+            world.s3Filepath,
+            flavor
+          )
+          */
+        };
+        cardFooterLink.href = "blah"; //TODO: find syntax.
+        cardFooterLink.appendChild(document.createTextNode(
+          "create world with this flavor..."
         ));
         cardFooter.appendChild(cardFooterLink);
   }
