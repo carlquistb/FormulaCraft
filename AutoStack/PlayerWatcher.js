5cabc24256dc73b325082f6969f571c1878a4322
lipian <lipian@cs.washington.edu>
2020-09-10 19:02:28 -0700

    Fixing jslint problems

--- AutoStack/PlayerWatcher.js
+++ AutoStack/PlayerWatcher.js
@@ -6,19 +6,19 @@ function PlayerWatcher(shutdownFunc) {
     this._updateInterval();
 }
 
-PlayerWatcher.prototype.registerWithStreamWatcher = function(streamWatcher) {
+PlayerWatcher.prototype.registerWithStreamWatcher = function (streamWatcher) {
     streamWatcher.addWatcher(
-    	/^\[\d\d:\d\d:\d\d\] \[Server thread\/INFO\]: ([\S]+) joined the game/, 
-    	(stdin, regexData) => this._addPlayer(stdin, regexData)
+        /^\[\d\d:\d\d:\d\d\] \[Server thread\/INFO\]: ([\S]+) joined the game/,
+        (stdin, regexData) => this._addPlayer(stdin, regexData)
     );
     streamWatcher.addWatcher(
-    	/^\[\d\d:\d\d:\d\d\] \[Server thread\/INFO\]: ([\S]+) left the game/, 
-    	(stdin, regexData) => this._removePlayer(stdin, regexData)
+        /^\[\d\d:\d\d:\d\d\] \[Server thread\/INFO\]: ([\S]+) left the game/,
+        (stdin, regexData) => this._removePlayer(stdin, regexData)
     );
 }
 
-PlayerWatcher.prototype._attemptShutdown = function() {
-	console.log(this._loggedInPlayers)
+PlayerWatcher.prototype._attemptShutdown = function () {
+    console.log(this._loggedInPlayers)
     if (this._loggedInPlayers.length === 0) {
         this._shutdownFunc.call();
     } else {
@@ -26,13 +26,13 @@ PlayerWatcher.prototype._attemptShutdown = function() {
     }
 }
 
-PlayerWatcher.prototype._addPlayer = function(stdin, regexData) {
+PlayerWatcher.prototype._addPlayer = function (stdin, regexData) {
     this._loggedInPlayers.push(regexData[1]);
     console.log(this._loggedInPlayers[0])
     this._updateInterval();
 }
 
-PlayerWatcher.prototype._removePlayer = function(stdin, regexData) {
+PlayerWatcher.prototype._removePlayer = function (stdin, regexData) {
     const playerIndex = this._loggedInPlayers.indexOf(regexData[1]);
     if (playerIndex > -1) {
         this._loggedInPlayers.splice(playerIndex, 1);
@@ -40,7 +40,7 @@ PlayerWatcher.prototype._removePlayer = function(stdin, regexData) {
     this._updateInterval();
 }
 
-PlayerWatcher.prototype._updateInterval = function() {
+PlayerWatcher.prototype._updateInterval = function () {
     if (this._shutdownInterval) {
         clearInterval(this._shutdownInterval);
     }
