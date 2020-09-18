0e043e075b552b5fb4cba58c93db7a688b46b270
NateThePlate <42iscool42@gmail.com>
2020-09-17 20:58:58 -0700

    Add Linter Workflow (#43)
    Adding a new lint and prettify workflow that will be run on pull requests. These can be run locally from the Autostack folder by running npm run lint. If the workflow detects fixable linting problems, these will be automatically commit to the branch as well. Some errors will still have to be fixed locally.

--- AutoStack/StreamWatcher.js
+++ AutoStack/StreamWatcher.js
@@ -1,6 +1,6 @@
 /**
- * A StreamWatcher object takes a process and monitors its standard out for 
- * certain user defined regex patterns.  When these patterns are seen, a 
+ * A StreamWatcher object takes a process and monitors its standard out for
+ * certain user defined regex patterns.  When these patterns are seen, a
  * registered callback function will be run.
  */
 
@@ -11,14 +11,15 @@ module.exports = StreamWatcher;
  * @param child_process the result of a call to child_process.spawn
  */
 function StreamWatcher(childProcess) {
-	if (!childProcess)
+	if (!childProcess) {
 		throw new Error("Invalid child process");
+	}
 
 	this._process = childProcess;
 	this._patterns = [];
 	this._callbacks = [];
-	this._process.stdout.on('data', (data) => this._onData(data));
-	this._process.stderr.on('data', (data) => this._onData(data));
+	this._process.stdout.on("data", (data) => this._onData(data));
+	this._process.stderr.on("data", (data) => this._onData(data));
 }
 
 /**
@@ -32,33 +33,35 @@ function StreamWatcher(childProcess) {
  *                  the pattern matches.
  */
 StreamWatcher.prototype.addWatcher = function (pattern, callback) {
-	if (!pattern)
+	if (!pattern) {
 		throw new Error("Invalid pattern");
-	if (!callback)
+	}
+	if (!callback) {
 		throw new Error("Invalid callback");
+	}
 
 	this._patterns.push(pattern);
 	this._callbacks.push(callback);
-}
+};
 
 /**
- * Adds a callback that will be executed when the child process finishes 
+ * Adds a callback that will be executed when the child process finishes
  * executing.
  *
  * @param callback The function to call on the child process's exit.
  */
 StreamWatcher.prototype.addOnExit = function (callback) {
 	this._process.on("exit", callback);
-}
+};
 
 StreamWatcher.prototype._onData = function (data) {
-	let strData = String(data);
+	const strData = String(data);
 	for (let i = 0; i < this._patterns.length; i++) {
-		let pattern = this._patterns[i];
-		let result = pattern.exec(strData);
+		const pattern = this._patterns[i];
+		const result = pattern.exec(strData);
 		if (result != null) {
-			let func = this._callbacks[i];
+			const func = this._callbacks[i];
 			func(this._process.stdin, result);
 		}
 	}
-}
+};
